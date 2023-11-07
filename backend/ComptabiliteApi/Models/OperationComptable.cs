afc68f8446c5dbdc9f985bac16367c7e2f6154a5
oussama hdidou <oussamahdidou15@gmail.com>
2023-11-07 23:36:20 +0100

    models

---
+++ backend/ComptabiliteApi/Models/OperationComptable.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComptabiliteAPi.Models
+{
+    public class OperationComptable
+    {
+        [Key]
+        int Id { get; set; }
+        string? compte_debiteurs { get; set; }
+        int code_compte_debiteurs { get; set; }
+        string? compte_crediteurs {  get; set; }
+        int code_compte_crediteurs { get; set; }
+        int id_facture {  get; set; }
+
+
+    }
+}
