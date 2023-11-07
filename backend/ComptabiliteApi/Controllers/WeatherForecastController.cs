de4ce8f1defecd344f225fe6b783c002fd7bed51
oussama hdidou <oussamahdidou15@gmail.com>
2023-11-07 15:05:37 +0100

    microservices

--- backend/ComptabiliteApi/Controllers/WeatherForecastController.cs
+++ backend/ComptabiliteApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace backend.Controllers;
+namespace ComptabiliteAPi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- backend/ComptabiliteApi/WeatherForecast.cs
+++ backend/ComptabiliteApi/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace backend;
+namespace ComptabiliteAPi;
 
 public class WeatherForecast
 {
