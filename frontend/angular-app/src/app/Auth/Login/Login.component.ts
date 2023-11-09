3bac5eba5fffcfdfabfb30f78a7c206793784d13
oussama hdidou <oussamahdidou15@gmail.com>
2023-11-09 20:48:35 +0100

    form

--- frontend/angular-app/src/app/Auth/Login/Login.component.ts
+++ frontend/angular-app/src/app/Auth/Login/Login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-Login',
   templateUrl: './Login.component.html',
-  styleUrls: ['./Login.component.css']
+  styleUrls: ['./Login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  myForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      // Define your form controls here
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
-
 }
--- frontend/angular-app/src/app/app.module.ts
+++ frontend/angular-app/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NotFoundComponentComponent } from './NotFoundComponent/NotFoundComponent.component';
 import { NotFoundComponent } from './NotFound/NotFound.component';
 
 @NgModule({
-  declarations: [		
-    AppComponent,
-      NotFoundComponentComponent,
-      NotFoundComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NotFoundComponent, NotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
